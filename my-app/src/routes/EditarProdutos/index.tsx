7c6626486b8148660e0505759c6c96f264f061fd
borbagustavo <gustavo.borba.27@gmail.com>
2026-09-24 20:46:11 -0300

    feat: methods: put, post, delete

--- my-app/src/routes/EditarProdutos/index.tsx
+++ my-app/src/routes/EditarProdutos/index.tsx
@@ -1,5 +1,7 @@
 
-import { useParams } from "react-router"
+import { useNavigate,useParams } from "react-router"
+import type { TipoProdutoJson } from "../../types/types";
+import { useState, useEffect } from "react";
 
 
 const listaProdutos = [
@@ -9,42 +11,88 @@ const listaProdutos = [
 ];
 
 export default function EditarProdutos() {
-
-
-
   document.title = "Editar Produtos"
-  const { id } = useParams<string>()
-  /*
-  //Uso do destructuring 
-  const estojo = {lapis: "Preto", caneta: "Azul", borracha: "Branca"}
-  
-  // aplicado o mesmo, para utilizar os atributos dos objetos.
-  const {lapis, caneta} = estojo;
-  
-  const jogos = ["Sonic", "Mario", "Zelda"]
-  
-  const [sonic, mario] = jogos  // ele pega posição do vetor, sendo assim [0] = sonic, [1] = mario, [2] = zelda
-  
-  */
+  // recuperando o id do produto da URL (params)
+  const { id } = useParams<{ id: string }>()
+  const navigate = useNavigate();
+  const [produto, setProduto] = useState<TipoProdutoJson>({ id: "", nome: "", preco: 0, estoque: 0, avatar: "" })
+  useEffect(() => {
+    // requisição para o backend apenas uma vez
+    const carregarProdutos = async () => {
+      try {
+        const response = await fetch(`http://localhost:3001/produtos/${id}`);
+        if (!response.ok) {
+          throw new Error(`Erro produto não encontrado: ${response.status} ${response.statusText}`);
+        }
+        const data: TipoProdutoJson = await response.json();
+        console.log("Produtos carregados:", data);
+        setProduto(data);
+      } catch (error) {
+        console.error("Erro ao carregar produtos:", error);
+      }
+    };
+    carregarProdutos();
+  }, [])
 
-  const produto = listaProdutos.find((p) => p.id === Number(id));
+  const handleUpdateProduto = async ()=>{
+      try {
+
+        const response = await fetch(`http://localhost:3001/produtos/${produto.id}` , {
+          method:"PUT",
+          headers:{
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(produto)
+        });
+
+              if (!response.ok) {
+                  throw new Error(`A atualização falhou: ${response.status} - ${response.statusText}`)
+              }
+
+              //MSG de SUCESSO
+              alert("Atualização realizada com sucesso!");
+              //Redirecionando para a página de produtos
+              navigate("/produtos");
+
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
   return (
     <main>
+      <h2>Editar Produtos</h2>
       <div>
-
-
-
-        {produto ? (
-          <div>
-            <p>Nome: {produto.nome}</p>
-            <p>Preço: {produto.preco}</p>
-          </div>
-        ) :
-          (<p>Produto não encontrado</p>)
-        }
-
+        <form>
+          <fieldset>
+            <legend>Dados do Produto</legend>
+            <div>
+              <label htmlFor="nomeProduto">Nome Produto </label>
+              <input type="text" name="nome" id="nomeProduto" value={produto.nome}
+                onChange={e => setProduto({ ...produto, nome: e.target.value })} />
+            </div>
+            <div>
+              <label htmlFor="preco">Preço R$ </label>
+              <input type="number" name="preco" id="preco" value={produto.preco} onChange={e => setProduto({ ...produto, preco: parseInt(e.target.value) })} />
+            </div>
+            <div>
+              <label htmlFor="estoque">Em estoque </label>
+              <input type="number" name="estoque" id="estoque" value={produto.estoque} onChange={e => setProduto({ ...produto, estoque: parseInt(e.target.value) })} />
+            </div>
+            <div>
+              <figure>
+                <img src={produto.avatar} alt={produto.nome} />
+                <figcaption>{produto.nome}</figcaption>
+              </figure>
+            </div>
+            <div>
+              <button type="button" onClick={handleUpdateProduto}>Editar</button>
+            </div>
+          </fieldset>
+        </form>
       </div>
-
     </main>
   )
 }
+
+
--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,66 +1,86 @@
 
-      import { useEffect, useState } from "react";
-      import type { TipoProdutoJson } from "../../types/types";
-      import { Link } from "react-router";
+import { useEffect, useState } from "react";
+import type { TipoProdutoJson } from "../../types/types";
+import { Link, useNavigate } from "react-router";
 
 
-      export default function Produtos() {
-        document.title = "Produtos"
+export default function Produtos() {
+  document.title = "Produtos"
 
-        const [produtos, setProdutos] = useState<TipoProdutoJson[]>([])
+  const [produtos, setProdutos] = useState<TipoProdutoJson[]>([])
 
-        useEffect(() => {
-          // requisição para o backend apenas uma vez
-          const carregarProdutos = async () => {
-            try {
-            const response = await fetch("http://localhost:3001/produtos");
-            if (!response.ok) {
-              throw new Error(`Erro na listagem de produtos: ${response.status} ${response.statusText}`);
-            }
-            const data: TipoProdutoJson[] = await response.json();
-            console.log("Produtos carregados:", data);
-            setProdutos(data);
-          }catch (error) {
-            console.error("Erro ao carregar produtos:", error);
-          }
-          };
-          carregarProdutos();
-          }, [])
-
-        return (
-          <main>
-            <h2>Produtos</h2>
-
-            <table border={1} cellPadding={10} style={{ width: '100%', borderCollapse: 'collapse' }}>
-              <thead>
-                <tr style={{ backgroundColor: '#be7310', color: '#050505' }}>
-                  <th>ID</th>
-                  <th>Nome</th>
-                  <th>Preço</th>
-                  <th>Descrição</th>
-                  <th>Avatar</th>
-                  <th>Editar</th>
-                </tr>
-              </thead>
-              <tbody>
-                {produtos.map((p) => (
-                  <tr key={p.id}>
-                    <td>{p.id}</td>
-                    <td>{p.nome}</td>
-                    <td>{p.preco.toFixed(2)}</td>
-                    <td>{p.estoque}</td>
-                    <td><img src={p.avatar} alt={p.nome} width={60} height={60} style={{ objectFit: 'cover' }} /></td>
-                    <td><Link to={`/produtos/${p.id}`}>Editar</Link></td>
-                  </tr>
-                ))}
-              </tbody>
-              <tfoot>
-              </tfoot>
-              <tr>
-                <td>Quantidade de produtos - {produtos.length}</td>
-              </tr>
-            </table>
-
-          </main>
-        )
+  useEffect(() => {
+    // requisição para o backend apenas uma vez
+    const carregarProdutos = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/produtos");
+        if (!response.ok) {
+          throw new Error(`Erro na listagem de produtos: ${response.status} ${response.statusText}`);
+        }
+        const data: TipoProdutoJson[] = await response.json();
+        console.log("Produtos carregados:", data);
+        setProdutos(data);
+      } catch (error) {
+        console.error("Erro ao carregar produtos:", error);
       }
+    };
+    carregarProdutos();
+  }, [])
+
+  const handleDeleteProduto = async (id: string) => {
+    try {
+      const response = await fetch(`http://localhost:3001/produtos/${id}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error(`Erro na exclusão do produto: ${response.status} ${response.statusText}`);
+      }
+
+      alert("Produto excluído com sucesso!");
+      const navigate = useNavigate();
+      navigate("/produtos");
+
+      // Atualizar a lista de produtos após a exclusão
+      setProdutos(produtos.filter((p) => p.id !== id));
+    } catch (error) {
+      console.error("Erro ao excluir produto:", error);
+    }
+  };
+
+  return (
+    <main>
+      <h2>Produtos</h2>
+
+      <table border={1} cellPadding={10} style={{ width: '100%', borderCollapse: 'collapse' }}>
+        <thead>
+          <tr style={{ backgroundColor: '#be7310', color: '#050505' }}>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Preço</th>
+            <th>Descrição</th>
+            <th>Avatar</th>
+            <th>Editar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {produtos.map((p) => (
+            <tr key={p.id}>
+              <td>{p.id}</td>
+              <td>{p.nome}</td>
+              <td>{p.preco.toFixed(2)}</td>
+              <td>{p.estoque}</td>
+              <td><img src={p.avatar} alt={p.nome} width={60} height={60} style={{ objectFit: 'cover' }} /></td>
+              <td><Link to={`/editar-produtos/${p.id}`}>Editar</Link> | <Link to={`#`} onClick={() => handleDeleteProduto(p.id)}>Deletar</Link></td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+        </tfoot>
+        <tr>
+          <td>Quantidade de produtos - {produtos.length}</td>
+        </tr>
+      </table>
+
+    </main >
+  )
+}
