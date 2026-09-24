b858f7519cbf12e2fed61503beeb514d6c24270e
borbagustavo <gustavo.borba.27@gmail.com>
2026-09-24 20:47:24 -0300

    fix: tirando excesso

--- my-app/src/routes/EditarProdutos/index.tsx
+++ my-app/src/routes/EditarProdutos/index.tsx
@@ -4,11 +4,7 @@ import type { TipoProdutoJson } from "../../types/types";
 import { useState, useEffect } from "react";
 
 
-const listaProdutos = [
-  { id: 1, nome: "Produto 1", preco: 10.0 },
-  { id: 2, nome: "Produto 2", preco: 20.0 },
-  { id: 3, nome: "Produto 3", preco: 30.0 },
-];
+
 
 export default function EditarProdutos() {
   document.title = "Editar Produtos"
